7dc5950e15d89cdca01b74bb3c1d5635c791b398
Paola <paola.stuardo.viveros@gmail.com>
2024-07-16 22:47:34 -0400

    Inicial commit

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+import "../../node_modules/jquery/dist/jquery.js";
+
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+
+$(document).ready(function () {
+    $("h6").dblclick(function () {
+        $(this).fadeOut(2000);
+
+    });
+});
+let alertContainer = document.getElementById("alertContainer");
+
+const alert = (message, type) => {
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    "</div>",
+  ].join("");
+
+  alertContainer.append(wrapper);
+};
+
+const alertTrigger = document.getElementById("alertBtn");
+if (alertTrigger) {
+  alertTrigger.addEventListener("click", () => {
+    alert("Mensaje enviado con éxito !!!", "success");
+  });
+}
+
+// Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
